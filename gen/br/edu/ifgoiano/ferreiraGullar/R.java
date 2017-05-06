71aba2d4a3ef2ea188ee9d5f31e2719d04285776
Igbobr <Igbobr@hotmail.com>
2017-05-06 18:45:40 -0300

    III.AtualizacaoHome
    Atualização para o material desing

--- gen/br/edu/ifgoiano/ferreiraGullar/R.java
+++ gen/br/edu/ifgoiano/ferreiraGullar/R.java
@@ -16,11 +16,12 @@ public final class R {
         public static final int titulo=0x7f020002;
     }
     public static final class id {
-        public static final int button1=0x7f060001;
-        public static final int button2=0x7f060002;
-        public static final int button3=0x7f060003;
-        public static final int button4=0x7f060004;
-        public static final int textView1=0x7f060000;
+        public static final int button1=0x7f060002;
+        public static final int button2=0x7f060003;
+        public static final int button3=0x7f060004;
+        public static final int button4=0x7f060005;
+        public static final int scrollView1=0x7f060000;
+        public static final int textView1=0x7f060001;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
@@ -45,8 +46,10 @@ public final class R {
 
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
+
+
     
- API 11 theme customizations can go here. 
+ Theme customizations available in newer API levels can go in res/values-vXX/styles.xml, while customizations related to backward-compatibility can go here. 
 
         Base application theme for API 14+. This theme completely replaces
         AppBaseTheme from BOTH res/values/styles.xml and
