a05aa3403e32f557a97240906ca5e5c2b5c5ce82
Igbobr <Igbobr@hotmail.com>
2017-05-06 17:26:42 -0300

    II. Criacao da Home
    Criei a home e o desing

--- gen/br/edu/ifgoiano/ferreiraGullar/R.java
+++ gen/br/edu/ifgoiano/ferreiraGullar/R.java
@@ -11,14 +11,26 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ferreira=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int titulo=0x7f020002;
+    }
+    public static final class id {
+        public static final int button1=0x7f060001;
+        public static final int button2=0x7f060002;
+        public static final int button3=0x7f060003;
+        public static final int button4=0x7f060004;
+        public static final int textView1=0x7f060000;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int bot1=0x7f040001;
+        public static final int bot2=0x7f040002;
+        public static final int bot3=0x7f040003;
+        public static final int bot4=0x7f040004;
     }
     public static final class style {
         /** 
