2f3683fa6d7212ea942fdbc5fef6af25124999a4
Igbobr <Igbobr@hotmail.com>
2017-05-07 15:03:51 -0300

    VIII. Telas da tela pre-quiz
    Insersão das imagens da tela pré-quiz

--- gen/br/edu/ifgoiano/ferreiraGullar/R.java
+++ gen/br/edu/ifgoiano/ferreiraGullar/R.java
@@ -11,26 +11,40 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ferreira=0x7f020000;
-        public static final int ferreirapb=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int layout_shadow_gray=0x7f020003;
-        public static final int titulo=0x7f020004;
+        public static final int bloq=0x7f020000;
+        public static final int bloqueado=0x7f020001;
+        public static final int bloqueadocor=0x7f020002;
+        public static final int ferreira=0x7f020003;
+        public static final int ferreirapb=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int layout_shadow_gray=0x7f020006;
+        public static final int locked=0x7f020007;
+        public static final int question=0x7f020008;
+        public static final int titulo=0x7f020009;
+        public static final int yes=0x7f02000a;
     }
     public static final class id {
-        public static final int botaoAvancar=0x7f06000c;
-        public static final int botaoVoltar=0x7f06000b;
+        public static final int botaoAvancar=0x7f060014;
+        public static final int botaoVoltar=0x7f060013;
         public static final int button1=0x7f060002;
         public static final int button2=0x7f060003;
         public static final int button3=0x7f060004;
         public static final int button4=0x7f060005;
-        public static final int enunciado=0x7f060007;
+        public static final int enunciado=0x7f06000f;
         public static final int layoutPergunta=0x7f060006;
-        public static final int rb01=0x7f060009;
-        public static final int rb02=0x7f06000a;
+        public static final int rb01=0x7f060011;
+        public static final int rb02=0x7f060012;
         public static final int scrollView1=0x7f060000;
-        public static final int teste=0x7f060008;
+        public static final int teste=0x7f060010;
         public static final int textView1=0x7f060001;
+        public static final int textView2=0x7f060007;
+        public static final int textView3=0x7f060008;
+        public static final int textView4=0x7f060009;
+        public static final int textView5=0x7f06000a;
+        public static final int textView6=0x7f06000b;
+        public static final int textView7=0x7f06000c;
+        public static final int textView8=0x7f06000d;
+        public static final int textView9=0x7f06000e;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
@@ -61,13 +75,15 @@ public final class R {
             backward-compatibility can go here.
         
 
-        Base application theme for API 11+. This theme completely replaces
-        AppBaseTheme from res/values/styles.xml on API 11+ devices.
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
 
-
-
-    
- Theme customizations available in newer API levels can go in res/values-vXX/styles.xml, while customizations related to backward-compatibility can go here. 
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
 
         Base application theme for API 14+. This theme completely replaces
         AppBaseTheme from BOTH res/values/styles.xml and
