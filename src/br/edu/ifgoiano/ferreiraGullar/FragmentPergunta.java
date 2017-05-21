8df9e14683b4247b3292188ccef99cad033cd966
Igbobr <Igbobr@hotmail.com>
2017-05-21 00:02:27 -0300

    XII. Pergunta tela quiz1
    Criação tela quiz 1

--- src/br/edu/ifgoiano/ferreiraGullar/FragmentPergunta.java
+++ src/br/edu/ifgoiano/ferreiraGullar/FragmentPergunta.java
@@ -7,23 +7,38 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
 public class FragmentPergunta extends Fragment {
-
+	int contador = 0;
+	private RadioGroup rg; 
 	
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-		View view = inflater.inflate(R.layout.layout_pergunta, container, false);
+		final View view = inflater.inflate(R.layout.layout_pergunta, container, false);
 
-		
+		rg = (RadioGroup) view.findViewById(R.id.quiz1radio);
+        rg.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged (RadioGroup group,int checkedId){
+            	RadioButton rb = (RadioButton) view.findViewById(checkedId);
+            	
+            
+            }
+        });
 		
 		Button buttonAvancar = (Button) view.findViewById(R.id.botaoAvancar);
 		buttonAvancar.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
-
+				if (contador  == 0){
+					
+				}
 			}
 		});
 
--- src/br/edu/ifgoiano/ferreiraGullar/SelecaoQuiz.java
+++ src/br/edu/ifgoiano/ferreiraGullar/SelecaoQuiz.java
@@ -1,14 +1,17 @@
 package br.edu.ifgoiano.ferreiraGullar;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
+
 
 public class SelecaoQuiz extends Activity {
-
+	private FragmentManager fm = getFragmentManager();
+	private LinearLayout l1 , l2;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -18,5 +21,25 @@ public class SelecaoQuiz extends Activity {
 		getWindow().getDecorView()
 	.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 		getWindow().setStatusBarColor(Color.TRANSPARENT);
+		l1 = (LinearLayout) findViewById(R.id.layout1);
+		l2 = (LinearLayout) findViewById(R.id.fragment);
+
+		
+	}
+
+	public void quiz1(View x){
+		
+		/*Tela01 t = new Tela01();
+					Bundle bundle = new Bundle();
+					bundle.putString("nome", "Rodrigo Gomide");
+					t.setArguments(bundle);
+					ft.replace(R.id.telaDireita, t);*/
+		FragmentTransaction ft = fm.beginTransaction();
+		FragmentPergunta f = new FragmentPergunta();
+		ft.replace(R.id.layout1, f);
+		ft.commit();
+		
+		
+		
 	}
 }
