05360a1f84971d3bfcabf61ebf250981c375dd5a
MathCassimiro <mathcassimiro@gmail.com>
2017-05-21 14:24:18 -0300

    XIII. Criação Fragment Poema
    Criado um fragment para colocar os poemas, e adicionado o primeiro poema
    (Traduzir-se)

--- gen/br/edu/ifgoiano/ferreiraGullar/R.java
+++ gen/br/edu/ifgoiano/ferreiraGullar/R.java
@@ -34,47 +34,52 @@ public final class R {
         public static final int yes=0x7f020010;
     }
     public static final class id {
-        public static final int botaoAvancar=0x7f070021;
-        public static final int botaoVoltar=0x7f070020;
-        public static final int buajuda=0x7f070005;
-        public static final int bubibli=0x7f070004;
-        public static final int bupoemas=0x7f070002;
-        public static final int buquiz=0x7f070003;
-        public static final int enunciado=0x7f07001b;
-        public static final int fragment=0x7f070011;
-        public static final int layout1=0x7f070007;
-        public static final int layoutPergunta=0x7f070006;
-        public static final int poema1=0x7f070012;
-        public static final int poema2=0x7f070013;
-        public static final int poema3=0x7f070014;
-        public static final int poema4=0x7f070015;
-        public static final int poema5=0x7f070016;
-        public static final int poema6=0x7f070017;
-        public static final int poema7=0x7f070018;
-        public static final int poema8=0x7f070019;
-        public static final int poema9=0x7f07001a;
-        public static final int quiz1=0x7f070008;
-        public static final int quiz1radio=0x7f07001c;
-        public static final int quiz2=0x7f070009;
-        public static final int quiz3=0x7f07000a;
-        public static final int quiz4=0x7f07000b;
-        public static final int quiz5=0x7f07000c;
-        public static final int quiz6=0x7f07000d;
-        public static final int quiz7=0x7f07000e;
-        public static final int quiz8=0x7f07000f;
-        public static final int quiz9=0x7f070010;
-        public static final int rb01=0x7f07001d;
-        public static final int rb02=0x7f07001e;
-        public static final int rb03=0x7f07001f;
-        public static final int scrollView1=0x7f070000;
-        public static final int textView1=0x7f070001;
+        public static final int botaoAvancar=0x7f070022;
+        public static final int botaoVoltar=0x7f070021;
+        public static final int buajuda=0x7f070006;
+        public static final int bubibli=0x7f070005;
+        public static final int bupoemas=0x7f070003;
+        public static final int buquiz=0x7f070004;
+        public static final int enunciado=0x7f07001c;
+        public static final int fragment=0x7f070012;
+        public static final int layout1=0x7f070008;
+        public static final int layoutPergunta=0x7f070007;
+        public static final int poema1=0x7f070013;
+        public static final int poema2=0x7f070014;
+        public static final int poema3=0x7f070015;
+        public static final int poema4=0x7f070016;
+        public static final int poema5=0x7f070017;
+        public static final int poema6=0x7f070018;
+        public static final int poema7=0x7f070019;
+        public static final int poema8=0x7f07001a;
+        public static final int poema9=0x7f07001b;
+        public static final int quiz1=0x7f070009;
+        public static final int quiz1radio=0x7f07001d;
+        public static final int quiz2=0x7f07000a;
+        public static final int quiz3=0x7f07000b;
+        public static final int quiz4=0x7f07000c;
+        public static final int quiz5=0x7f07000d;
+        public static final int quiz6=0x7f07000e;
+        public static final int quiz7=0x7f07000f;
+        public static final int quiz8=0x7f070010;
+        public static final int quiz9=0x7f070011;
+        public static final int rb01=0x7f07001e;
+        public static final int rb02=0x7f07001f;
+        public static final int rb03=0x7f070020;
+        public static final int scrollView1=0x7f070001;
+        public static final int telaPoema=0x7f070000;
+        public static final int textView1=0x7f070002;
+        public static final int textoPoema=0x7f070024;
+        public static final int tituloPoema=0x7f070023;
     }
     public static final class layout {
-        public static final int activity_home=0x7f030000;
-        public static final int activity_perguntas=0x7f030001;
-        public static final int activity_selecao_quiz=0x7f030002;
-        public static final int activity_selecionar_poemas=0x7f030003;
-        public static final int layout_pergunta=0x7f030004;
+        public static final int activity_activity_poema=0x7f030000;
+        public static final int activity_home=0x7f030001;
+        public static final int activity_perguntas=0x7f030002;
+        public static final int activity_selecao_quiz=0x7f030003;
+        public static final int activity_selecionar_poemas=0x7f030004;
+        public static final int layout_pergunta=0x7f030005;
+        public static final int layout_poema=0x7f030006;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
@@ -83,11 +88,16 @@ public final class R {
         public static final int bot3=0x7f050003;
         public static final int bot4=0x7f050004;
         public static final int hello_world=0x7f050007;
+        public static final int title_activity_activity_poema=0x7f05000b;
         public static final int title_activity_pergunta=0x7f050006;
         public static final int title_activity_perguntas=0x7f050008;
         public static final int title_activity_selecao_quiz=0x7f050009;
         public static final int title_activity_selecionar_poemas=0x7f05000a;
         public static final int titulo=0x7f050005;
+        /**  Poemas 
+         */
+        public static final int titulo_traduzirse=0x7f05000c;
+        public static final int traduzirse=0x7f05000d;
     }
     public static final class style {
         /** 
@@ -104,11 +114,13 @@ public final class R {
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
 
+
         
 
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
+
 
         
 
